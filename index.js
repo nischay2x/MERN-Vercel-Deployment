e08eaa3c71d7f1cee39d25af31b2f3934c8400bd
nischay2x <77281955+nischay2x@users.noreply.github.com>
2022-01-08 16:53:38 +0530

    cors blocked

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+// app.use(cors());
 
 app.get("/api/test", (req, res) => {
   res.send("test");
